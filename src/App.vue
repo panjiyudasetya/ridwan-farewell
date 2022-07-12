03560dffe782efdb325d4a68da58f3e043cf1481
alamse <alamyudi@live.com>
2022-07-12 18:07:13 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,213 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Bejo"
+      class="logo"
+      src="./assets/ridwan.png"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="Daebakkkk!" />
+      <div class="flex-container">
+        <div class="time-unite created" v-show="days">
+          <div class="time-unite-title">Days</div>
+          <div class="time-unite-value">{{ days }}</div>
+        </div>
+        <div class="time-unite created">
+          <div class="time-unite-title">Hours</div>
+          <div class="time-unite-value">{{ hours }}</div>
+        </div>
+        <div class="time-unite created">
+          <div class="time-unite-title">Minutes</div>
+          <div class="time-unite-value">{{ minutes }}</div>
+        </div>
+        <div class="time-unite created">
+          <div class="time-unite-title">Seconds</div>
+          <div class="time-unite-value">{{ seconds }}</div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <div class="main-content">
+      <div class="marquee">
+        <TheWelcome />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  data() {
+    return {
+      actualTime: moment().format("X"),
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  },
+  methods: {
+    addOneSecondToActualTimeEverySecond() {
+      var component = this;
+      component.actualTime = moment().format("X");
+      setTimeout(function () {
+        component.addOneSecondToActualTimeEverySecond();
+      }, 1000);
+    },
+    getDiffInSeconds() {
+      return moment("2022-07-15 18:00:00").format("X") - this.actualTime;
+    },
+    compute() {
+      var duration = moment.duration(this.getDiffInSeconds(), "seconds");
+      this.days = duration.days() > 0 ? duration.days() : 0;
+      this.hours = duration.hours() > 0 ? duration.hours() : 0;
+      this.minutes = duration.minutes() > 0 ? duration.minutes() : 0;
+      this.seconds = duration.seconds() > 0 ? duration.seconds() : 0;
+    },
+  },
+  created() {
+    this.compute();
+    this.addOneSecondToActualTimeEverySecond();
+  },
+  watch: {
+    actualTime() {
+      this.compute();
+    },
+  },
+};
+</script>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1;
+  padding-right: 10px;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: #006064;
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: 2rem;
+    /* padding-right: calc(var(--section-gap) / 2); */
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+
+.flex-container {
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: #00838f;
+}
+
+.flex-container > div {
+  background-color: #eceff1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+}
+
+.time-unite-title {
+  margin-top: 10px;
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.time-unite-value {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  font-size: 50px;
+  font-weight: 500;
+}
+
+.main-content {
+  padding-right: 8px;
+  height: 100vh;
+  padding-left: 30px;
+  overflow: hidden;
+  position: relative;
+}
+
+.marquee {
+  /* box-sizing: border-box; */
+  animation: marquee 12s linear infinite;
+  display: block;
+  /* overflow: hidden; */
+  padding-left: 30px;
+  /* height: 100vh; */
+  position: absolute;
+}
+
+.marquee:hover {
+  animation-play-state: paused;
+}
+
+/* Make it move! */
+@keyframes marquee {
+  10% {
+    top: 0;
+  }
+  100% {
+    top: -100%;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,57 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+</script>
+
+<template>
+  <WelcomeItem v-for="(item, i) in messages" :key="i">
+    <template #icon>
+      <img :alt="item.from" :src="item.icon" width="35" height="35" />
+    </template>
+
+    <blockquote>
+      {{ item.message }}
+      <span>{{ item.from }}</span>
+    </blockquote>
+  </WelcomeItem>
+</template>
+<script>
+import messageData from "../data/messages.json";
+
+export default {
+  data() {
+    return {
+      messages: messageData,
+    };
+  },
+};
+</script>
+<style>
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic);
+blockquote {
+  font-size: 1.2em;
+  font-family: Open Sans;
+  font-style: italic;
+  line-height: 1.7em;
+}
+
+blockquote::before {
+  font-family: Arial;
+  content: "\201C";
+  color: #78c0a8;
+  font-size: 4em;
+  left: -5px;
+  top: 24px;
+}
+
+blockquote::after {
+  content: "";
+}
+
+blockquote span {
+  display: block;
+  color: #333333;
+  font-style: normal;
+  font-weight: bold;
+  margin-top: 1em;
+}
+</style>
